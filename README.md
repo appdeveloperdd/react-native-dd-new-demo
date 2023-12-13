a6250119a1cc49d3a40907c17d8178a42bae9e0f
appdeveloperdd <appdeveloper.dd@gmail.com>
2023-12-13 18:53:51 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-dd-new-demo
+
+Demo
+
+## Installation
+
+```sh
+npm install react-native-dd-new-demo
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-dd-new-demo";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativeddnewdemo/DdNewDemoModule.kt
@@ -0,0 +1,24 @@
+package com.reactnativeddnewdemo
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class DdNewDemoModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "DdNewDemo"
+    }
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Int, b: Int, promise: Promise) {
+    
+      promise.resolve(a * b)
+    
+    }
+
+    
+}
