c6c1b6aac82d887b2219409ab88e818fde82983a
appdeveloperdd <appdeveloper.dd@gmail.com>
2023-12-14 16:00:54 +0530

    feat: ios pod issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+'use strict';
+import Klaviyo from './Klaviyo';
 
-const LINKING_ERROR =
-  `The package 'react-native-dd-new-demo' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
-
-const DdNewDemo = NativeModules.DdNewDemo
-  ? NativeModules.DdNewDemo
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return DdNewDemo.multiply(a, b);
-}
+export default Klaviyo;
